6e8621c35c9b43de159c1afd0f4d9611837bbc3e
jbrondex <julien.brondex@univ-grenoble-alpes.fr>
2024-04-25 17:08:21 +0200

    Documentation of already existing codes

--- LatinHypercubeSampling_DamageParameters.py
+++ LatinHypercubeSampling_DamageParameters.py
@@ -14,20 +14,31 @@ from smt.sampling_methods import LHS
 ###Define parameter space
 Sigmath_limits = [0.05, 0.2] ###Values deduced from Teterousse steady state with empty cavity (2010 geometry) compared to crevasses of August 2011
 B_limits = [0.5, 2.0] ###Values of Krug 2014
-Lambdah_limits = [0.0, 0.5] ###0.4 in Pralong 2006
-xlimits = np.array([Sigmath_limits, B_limits, Lambdah_limits])
+###For lambdah we consider 3 discrete values
+Lambdah_values = [0.0, 0.5, 1.0] ###0.4 in Pralong 2006
+xlimits = np.array([Sigmath_limits, B_limits])
 sampling = LHS(xlimits=xlimits)
 
-### How many combinations do we want to consider ?
-num = 20
+### How many combinations (Sigmath, B) do we want to consider for each value of lambdah ?
+num = 8
 x = sampling(num)
 print(x.shape)
+
+### Create vectors X, Y, Z: X,Y are sampled values of Sigmath,B while Z are discrete values of lambdah
+X=[]
+Y=[]
+Z=[]
+for i,lambdah in enumerate(Lambdah_values):
+    X.append(x[:, 0])
+    Y.append(x[:, 1])
+    z=lambdah*np.ones((1,num))
+    Z.append(z)
 
 ### Show combinations in a Fig.
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
-ax.scatter(x[:, 0], x[:, 1], x[:, 2],"o")
+ax.scatter(X, Y, Z,"o")
 ax.set_xlabel(r'$\sigma_{th}~(\mathrm{MPa})$')
 ax.set_ylabel(r'$B~\mathrm{(MPa^{-1}~a^{-1})}$')
 ax.set_zlabel(r'$\lambda_h~\mathrm{(_)}$')
