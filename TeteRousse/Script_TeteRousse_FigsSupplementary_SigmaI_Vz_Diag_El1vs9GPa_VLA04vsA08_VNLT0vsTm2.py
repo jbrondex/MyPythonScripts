de2c6903031123e7e594ef6cf0b555d3b8dbd150
jbrondex <julien.brondex@univ-grenoble-alpes.fr>
2025-04-30 14:55:11 +0200

    Remove commented lines

--- TeteRousse/Script_TeteRousse_FigsSupplementary_SigmaI_Vz_Diag_El1vs9GPa_VLA04vsA08_VNLT0vsTm2.py
+++ TeteRousse/Script_TeteRousse_FigsSupplementary_SigmaI_Vz_Diag_El1vs9GPa_VLA04vsA08_VNLT0vsTm2.py
@@ -323,7 +323,7 @@ if __name__ == "__main__":
                 patch = PathPatch(clippath, facecolor='none')
                 # ax = plt.gca()
                 ax.add_patch(patch)
-                for c in C1.collections:
+                for c in [C1]:
                     c.set_clip_path(patch)
                 ##plot crevasses as continuous line
                 for crev_num in np.arange(Df_Crevasses['Crevasse Number'].min(), Df_Crevasses['Crevasse Number'].max() + 1):
@@ -344,102 +344,3 @@ if __name__ == "__main__":
         add_horizontal_colorbar(fig, CS_stiff_disp, [axes[1, 0], axes[1, 1], axes[1, 2]], ColBarLabel, levs_ticks_disp, x_offset=-0.01, y_offset=-0.137)
 
         plt.show()
-
-
-
-                # ###Make a horizontal color bar common to all subplots
-                # # Create a colorbar axis below all subplots
-                # cbar_ax = fig.add_axes([0.2, 0.08, 0.6, 0.03])  # [left, bottom, width, height]
-                # # Create a single colorbar for all subplots
-                # cbar = plt.colorbar(CS1, cax=cbar_ax, ticks=levs_ticks, orientation='horizontal')
-                # cbar.set_label(r'$\sigma_\mathrm{I}$ [kPa]', fontsize=20)
-                # cbar.ax.tick_params(labelsize=18)  # Adjust tick label size
-
-
-
-
-
-
-
-
-
-    #
-    # # #####################################################################
-    # # ###     PLOT MAP OF FIRST MAXI PRINCIPAL STRESS AT THE SURFACE    ###
-    # # #####################################################################
-    #
-    # ###Prepare the subplot
-    # nrows = 1
-    # ncols = 3  ###elastic, viscous, observed
-    # fig, axes = plt.subplots(nrows, ncols, figsize=(15, 12), sharex=True, sharey=True, constrained_layout=True)
-    #
-    # ###### START PLOT ######
-    # for k,law in enumerate(['El','VL','VNL']):
-    #     df_cav =pd.DataFrame()
-    #     df_nocav=pd.DataFrame()
-    #     if law == 'El':
-    #         df_nocav =  Data_Simu_El[Data_Simu_El['Case']=='NoCavity']
-    #         df_cav = Data_Simu_El[Data_Simu_El['Case']=='EmptyCavity']
-    #         Title = 'Elastic'
-    #     elif law == 'VL':
-    #         df_nocav =  Data_Simu_VL[Data_Simu_VL['Case']=='NoCavity']
-    #         df_cav = Data_Simu_VL[Data_Simu_VL['Case']=='EmptyCavity']
-    #         Title = 'Linear Viscous'
-    #     elif law=='VNL':
-    #         df_nocav =  Data_Simu_VNL[Data_Simu_VNL['Case']=='NoCavity']
-    #         df_cav = Data_Simu_VNL[Data_Simu_VNL['Case']=='EmptyCavity']
-    #         Title = 'Glen\'s law'
-    #     ##get proper ax
-    #     ax = axes[k]
-    #     ax.set_xlabel(r'X [km]', fontsize=22)
-    #     ax.set_ylabel(r'Y [km]', fontsize=22)
-    #     ax.tick_params(labelsize=18)  # fontsize of the tick labels
-    #     ax.set_xlabel("")
-    #     ax.set_ylabel("")
-    #     ax.set_xticklabels([])  # Remove x labels
-    #     ax.set_yticklabels([])  # Remove y labels
-    #     ax.grid(True)
-    #     ax.grid(alpha=0.5)
-    #     ax.set_title(Title, fontsize=21, weight='bold')
-    #     ###Force x and y axis to same scale
-    #     ax.set_aspect('equal', adjustable='box')
-    #     ###Poject SigmaI on interpolation grid
-    #     SigmaI_NoCav = Interpolate_field(df_nocav, 'SigmaI', X, Y)
-    #     SigmaI_Cav = Interpolate_field(df_cav, 'SigmaI', X, Y)
-    #     ###shading
-    #     clevs=np.arange(-0.01,121,10) ## cbar for shading
-    #     #colorbar
-    #     levs_ticks=np.arange(0.0,121,20)
-    #     ###Fills up the map with colors for SigmaEq
-    #     CS1 = ax.contourf(X/1000, Y/1000, (SigmaI_Cav-SigmaI_NoCav)*1000, clevs, cmap=cmap,extend='both')
-    #     ax.plot(xc / 1000, yc / 1000, color='k', linewidth=2)
-    #     ###Below we remove colors that are outside of glacier contour
-    #     clippath = mpltPath(np.c_[xc/1000, yc/1000])
-    #     patch = PathPatch(clippath, facecolor='none')
-    #     # ax = plt.gca()
-    #     ax.add_patch(patch)
-    #     for c in CS1.collections:
-    #         c.set_clip_path(patch)
-    #     ##plot crevasses as continuous line
-    #     for crev_num in np.arange(Df_Crevasses['Crevasse Number'].min(), Df_Crevasses['Crevasse Number'].max() + 1):
-    #         ###get proper points
-    #         Df_plot = Df_Crevasses[Df_Crevasses['Crevasse Number'] == crev_num]
-    #         if Df_plot['IsCircular'].all():  ##different colors for circular crevasses and other crevasses
-    #             col = Col_Crevasses_Circ
-    #         else:
-    #             col = Col_Crevasses_Other
-    #         ax.plot(Df_plot['X'].values/1000, Df_plot['Y'].values/1000, color=col, linestyle='-', linewidth=2)
-    #     ###Plot cavity contour
-    #     ax.plot(cavity_contour[:, 0]/1000,cavity_contour[:, 1]/1000,color=Col_Cavity,linestyle='-',linewidth=2.5)
-    #     ###Make a horizontal color bar common to all subplots
-    #     # Create a colorbar axis below all subplots
-    #     cbar_ax = fig.add_axes([0.2, 0.08, 0.6, 0.03])  # [left, bottom, width, height]
-    #     # Create a single colorbar for all subplots
-    #     cbar = plt.colorbar(CS1, cax=cbar_ax, ticks=levs_ticks, orientation='horizontal')
-    #     cbar.set_label(r'$\sigma_\mathrm{I}$ [kPa]', fontsize=20)
-    #     cbar.ax.tick_params(labelsize=18)  # Adjust tick label size
-    #
-    #
-    # ###Show map
-    # plt.show()
-
